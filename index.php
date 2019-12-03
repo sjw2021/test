9066026e68f894b4eeb745b9989c7470e36a41b0
shijianwei <shijianwei@huaxuezoo.com>
2019-12-03 16:11:36 +0800

    上传

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ define('PACKAGE_DIR', ROOT_PATH.'/package');
 require PACKAGE_DIR.'/Autoload.php';
 require ROOT_PATH.'/functions.php';
 new Autoload();
+error_reporting(E_ALL);
 try {
 	new \app\Index();
 } catch (\Exception $e) {
