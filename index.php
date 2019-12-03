fff3998e64c0ac3109de7cfb1eb470e36f0c3257
shijianwei <shijianwei@huaxuezoo.com>
2019-12-03 17:45:08 +0800

    配置

--- index.php
+++ index.php
@@ -6,18 +6,18 @@ require PACKAGE_DIR.'/Autoload.php';
 require ROOT_PATH.'/functions.php';
 // 加载类
 new Autoload();
-$lockFile = ROOT_PATH.'/db.lock';
-if (!file_exists($lockFile)) {
-	$db = \package\Db::instance();
-	$res = $db->query(file_get_contents(ROOT_PATH.'/db.sql'));
-	if ($res) {
-		file_put_contents($lockFile, '1');
-	}else{
-		json_error('数据库写入出错');
-	}
-}
 try {
+	$lockFile = ROOT_PATH.'/db.lock';
+	if (!file_exists($lockFile)) {
+		$db = \package\Db::instance();
+		$res = $db->query(file_get_contents(ROOT_PATH.'/db.sql'));
+		if ($res) {
+			file_put_contents($lockFile, '1');
+		}else{
+			throw new \Exception('数据库写入出错');
+		}
+	}
 	new \app\Index();
 } catch (\Exception $e) {
-	json_error($e->getMessage());
+	json_error($e->getCode() ?: 1,$e->getMessage());
 }
